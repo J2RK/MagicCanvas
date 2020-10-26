9b963f50961b7d29fc2e0f92294afa4589387e67
Reno <renovatio0424@gmail.com>
2020-10-26 12:57:57 +0900

    init commits

---
+++ sample/app/src/main/java/com/j2rk/magiccanvas/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.j2rk.magiccanvas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
